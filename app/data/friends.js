c05b5fff6e83eef02fa13ad9ec060c5932f26ac8
Patrick <patrickawong@gmail.com>
2017-11-10 18:34:32 -0800

    working

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+var friendsData=[{
+	"name" : "Tisha",
+	"photo" : "https://s-media-cache-ak0.pinimg.com/originals/14/52/21/14522119ab30fc8d34ac54731c28b898.jpg",
+	"scores" : [5,4,3,5,1,3,2,1,3,5]
+},{
+	"name" : "Misha",
+	"photo" : "http://www.punjabigraphics.com/images/158/Sweet-Kitten-Animal-Image.jpg",
+	"scores" : [1,4,3,5,1,1,2,1,2,3]
+},
+{	
+	"name" : "ABC",
+	"photo" : "https://www.spirit-animals.com/wp-content/uploads/2012/12/monkey.jpg",
+	"scores" : [2,3,4,5,1,2,5,2,1,4]
+},
+{
+	"name" : "xyz",
+	"photo" : "http://cdn.attackofthecute.com/January-21-2015-23-24-59-tt.jpg",
+	"scores" : [2,3,4,5,1,1,5,2,5,4]
+},
+ {
+	 "name" : "test",
+	"photo" : "http://vignette2.wikia.nocookie.net/pkmncollectors/images/0/02/009Blastoise.png/revision/latest?cb=20100612002433",
+	"scores" : [2,3,4,5,1,4,5,2,5,4]
+	
+		 }];
+module.exports = friendsData;
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,19 @@
+var path = require("path");
+var express = require("express");
+
+module.exports = function(app) {
+    app.get("/survey", function(req, res) {
+        // body...
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+    app.use("/", function(req, res) {
+        // body...
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
+    app.use('/css', function (req, res) {
+    	// body...
+    	res.sendFile(path.join(__dirname, "../public/assets/css/style.css"));
+    } )
+}
